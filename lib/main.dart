9787c673b04a7cb584d5220e9ff634c0e264222a
Melissa Heredia <herediamelissa19@gmail.com>
2020-07-23 10:47:37 -0400

    Basic Magic 8 Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      MaterialApp(home: Magic8BallPage()),
+    );
+
+class Magic8BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          centerTitle: false,
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Magic8Ball());
+  }
+}
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  var ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
       ),
     );
+  }
+}
